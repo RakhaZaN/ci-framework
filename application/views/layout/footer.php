68c7b3c3977cb6ec147bce29867430d33eb682f8
rakhazan <rakhazahnur26@gmail.com>
2022-06-09 18:06:56 +0700

    :lipstick: remove unused menu in sidebar

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,7 +1,7 @@
 </div>
-<script src="<?= base_url() ?>public/assets/js/app.js"></script>
+<script src="<?= base_url('public/assets/js/app.js') ?>"></script>
 
-<script src="<?= base_url() ?>public/assets/js/pages/dashboard.js"></script>
+<script src="<?= base_url('public/assets/js/pages/dashboard.js') ?>"></script>
 
 </body>
 
